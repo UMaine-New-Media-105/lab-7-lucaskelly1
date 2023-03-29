e19a30ebadabb036f93baeab85ed33b29fa69634
lucaskelly1 <124173836+lucaskelly1@users.noreply.github.com>
2023-03-29 10:38:02 -0400

    Create questionone.js
    Diagonal squares with random colors

---
+++ questionone.js
@@ -0,0 +1,15 @@
+let X;
+let nameOfArray = ["red", "green", "blue"];
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background("black");
+
+  for (let someVar = 0; someVar < 16; someVar++) {
+    fill(random(nameOfArray));
+    rect(someVar * 25, someVar * 25, 25);
+  }
+}
